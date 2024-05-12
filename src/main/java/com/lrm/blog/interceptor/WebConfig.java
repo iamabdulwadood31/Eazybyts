96e67c875440a653e8e2912d36ecf0cb84a56057
iamabdulwadood31 <133665118+iamabdulwadood31@users.noreply.github.com>
2024-05-12 18:31:42 +0530

    Update WebConfig.java

--- src/main/java/com/lrm/blog/interceptor/WebConfig.java
+++ src/main/java/com/lrm/blog/interceptor/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//有效的配置类
+
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
