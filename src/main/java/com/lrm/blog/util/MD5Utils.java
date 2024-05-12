344b84d9a1bf656c933e98bcb69f183f734e9796
iamabdulwadood31 <133665118+iamabdulwadood31@users.noreply.github.com>
2024-05-12 18:38:22 +0530

    Update MD5Utils.java

--- src/main/java/com/lrm/blog/util/MD5Utils.java
+++ src/main/java/com/lrm/blog/util/MD5Utils.java
@@ -5,11 +5,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class MD5Utils {
 
-    /**
-     * MD5加密类
-     * @param str 要加密的字符串
-     * @return    加密后的字符串
-     */
+   
     public static String code(String str){
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
@@ -25,10 +21,9 @@ public class MD5Utils {
                     buf.append("0");
                 buf.append(Integer.toHexString(i));
             }
-            //32位加密
+           
             return buf.toString();
-            // 16位的加密
-            //return buf.toString().substring(8, 24);
+           
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             return null;
