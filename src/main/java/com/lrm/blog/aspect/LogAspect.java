6bd57ccb59dca8f8032cb660cc472bdc6043101e
iamabdulwadood31 <133665118+iamabdulwadood31@users.noreply.github.com>
2024-05-12 18:28:32 +0530

    Update LogAspect.java

--- src/main/java/com/lrm/blog/aspect/LogAspect.java
+++ src/main/java/com/lrm/blog/aspect/LogAspect.java
@@ -16,16 +16,15 @@ import java.util.Arrays;
 public class LogAspect {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    //execution() - 声明它是一个切面，来规定拦截哪些类
-    //web的所有类
+    
     @Pointcut("execution(* com.lrm.blog.web.*.*(..))")
     public void log(){
 
     }
-    //在切面之前执行
+   
     @Before("log()")
     public void doBefore(JoinPoint joinPoint){
-        //获取HTTP request
+       
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest(); //request for attributes
         String url = request.getRequestURL().toString();  //需要toString 一下
@@ -37,14 +36,14 @@ public class LogAspect {
         logger.info("Request : {}",requestLog);
     }
 
-    //在切面之后执行
+    
     @After("log()")
     public void doAfter(){
 
         logger.info("-----doAfter-----");
     }
 
-    //在返回之后拦截
+   
     @AfterReturning(returning = "result", pointcut = "log()") // 定义result这个参数作为返回值
     public void doAfterReturn(Object result){
         logger.info("Result : " + result);
@@ -56,7 +55,7 @@ public class LogAspect {
         private String classMethod;
         private Object[] args;  //对象数组
 
-        //全参构造
+      
         public RequestLog(String url, String ip, String classMethod, Object[] args) {
             this.url = url;
             this.ip = ip;
@@ -64,7 +63,7 @@ public class LogAspect {
             this.args = args;
         }
 
-        //toString - 转换成字符串
+        
         @Override
         public String toString() {
             return "RequestLog{" +
