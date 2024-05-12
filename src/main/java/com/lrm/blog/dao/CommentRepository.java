01dc5222378ad4de233b5fc01f97209008034eee
iamabdulwadood31 <133665118+iamabdulwadood31@users.noreply.github.com>
2024-05-12 18:29:32 +0530

    Update CommentRepository.java

--- src/main/java/com/lrm/blog/dao/CommentRepository.java
+++ src/main/java/com/lrm/blog/dao/CommentRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    //Sort - 从最新往下排序
+    
      List<Comment> findByBlogIdAndParentCommentNull(Long blogId, Sort sort);
 }
