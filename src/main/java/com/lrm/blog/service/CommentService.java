e6f1df6764a18d1f93f484ff643ecb5ef1a39287
iamabdulwadood31 <133665118+iamabdulwadood31@users.noreply.github.com>
2024-05-12 18:34:54 +0530

    Update CommentService.java

--- src/main/java/com/lrm/blog/service/CommentService.java
+++ src/main/java/com/lrm/blog/service/CommentService.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public interface CommentService {
 
-    //获取列表
+    
     List<Comment> listCommentByBlogId(Long blogId);
 
-    //保存Comment对象
+    
     Comment saveComment(Comment comment);
 
 }
