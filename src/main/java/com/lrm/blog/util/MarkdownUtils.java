18f128acb1b25d9875fdc5504a9a496c3e0cf73a
iamabdulwadood31 <133665118+iamabdulwadood31@users.noreply.github.com>
2024-05-12 18:40:59 +0530

    Update MarkdownUtils.java

--- src/main/java/com/lrm/blog/util/MarkdownUtils.java
+++ src/main/java/com/lrm/blog/util/MarkdownUtils.java
@@ -14,16 +14,10 @@ import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.util.*;
 
-/**
- * Created by limi on 2017/10/22.
- */
-public class MarkdownUtils {
 
-    /**
-     * markdown格式转换成HTML格式
-     * @param markdown
-     * @return
-     */
+public class MarkdownUtils {
+return
+  
     public static String markdownToHtml(String markdown) {
         Parser parser = Parser.builder().build();
         Node document = parser.parse(markdown);
@@ -31,16 +25,11 @@ public class MarkdownUtils {
         return renderer.render(document);
     }
 
-    /**
-     * 增加扩展[标题锚点，表格生成]
-     * Markdown转换成HTML
-     * @param markdown
-     * @return
-     */
+  
     public static String markdownToHtmlExtensions(String markdown) {
-        //h标题生成id
+    
         Set<Extension> headingAnchorExtensions = Collections.singleton(HeadingAnchorExtension.create());
-        //转换table的HTML
+       
         List<Extension> tableExtension = Arrays.asList(TablesExtension.create());
         Parser parser = Parser.builder()
                 .extensions(tableExtension)
@@ -58,13 +47,11 @@ public class MarkdownUtils {
         return renderer.render(document);
     }
 
-    /**
-     * 处理标签的属性
-     */
+    
     static class CustomAttributeProvider implements AttributeProvider {
         @Override
         public void setAttributes(Node node, String tagName, Map<String, String> attributes) {
-            //改变a标签的target属性为_blank
+        
             if (node instanceof Link) {
                 attributes.put("target", "_blank");
             }
@@ -76,12 +63,9 @@ public class MarkdownUtils {
 
 
     public static void main(String[] args) {
-        String table = "| hello | hi   | 哈哈哈   |\n" +
-                "| ----- | ---- | ----- |\n" +
-                "| 斯维尔多  | 士大夫  | f啊    |\n" +
-                "| 阿什顿发  | 非固定杆 | 撒阿什顿发 |\n" +
-                "\n";
-        String a = "[imCoding 爱编程](http://www.lirenmi.cn)";
+        String table = "| hello | hi   |\n"
+               
+        String a = "[imCoding (http://www.lirenmi.cn)";
         System.out.println(markdownToHtmlExtensions(a));
     }
 }
